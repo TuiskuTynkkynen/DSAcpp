94c30979bfdc109c0f9bca223b0387c4587a0a1b
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2023-10-31 22:45:40 +0200

    Added Doubly Linked List class to linkedlist.h

--- DatatypesAndAlgorithms/linkedlist.h
+++ DatatypesAndAlgorithms/linkedlist.h
@@ -18,4 +18,24 @@ private:
 	Node* head = nullptr;
 };
 
+class DoublyLinkedList {
+public:
+	~DoublyLinkedList();
+	void PrependNode(int val);
+	void AppendNode(int val);
+	void InsertAt(int pos, int val);
+	void DeleteAt(int pos);
+	void DeleteValue(int val);
+	void FreeList();
+	void PrintList();
+	int Count();
+	int IndexOf(int val);
+private:
+	struct Node;
+	std::shared_ptr<Node> head = nullptr;
+	std::shared_ptr<Node> tail = nullptr;
+};
+
+
+
 #endif
