b9b591c58e06c3e61aacc785ead52a6ccabc29fd
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2023-11-03 21:22:35 +0200

    Added Min Heap definitions

--- DatatypesAndAlgorithms/heap.cpp
+++ DatatypesAndAlgorithms/heap.cpp
@@ -1,1 +1,77 @@
 #include "heap.h"
+#include <vector>
+#include <iostream>
+
+	heap::MinHeap::MinHeap(int size) {
+		heap = std::vector<int>(size);
+	}
+
+	heap::MinHeap::~MinHeap() {
+	}
+
+	heap::MinHeap heap::MinHeap::operator[](int index){
+		return heap[index];
+	}
+
+	void heap::MinHeap::Insert(int val) {
+		heap.push_back(val);
+		HeapifyUp();
+	}
+	
+	int heap::MinHeap::Delete() {
+		heap.erase(heap.begin());
+		HeapifyDown();
+	}
+
+	int heap::MinHeap::GetLength() {
+		return heap.size();
+	}
+
+	void heap::MinHeap::Print() {
+		int length = heap.size();
+		std::cout << "Heap = ";
+		for (int i = 0; i < length; i++) {
+			std::cout << heap[i] << ", ";
+		}
+		std::cout << "\n";
+	}
+
+	void heap::MinHeap::HeapifyDown() {
+		int index = heap.size();
+		int parentIndex;
+		while (index >= 0){
+			parentIndex = (index - 1) / 2;
+
+			if (heap[index] > heap[parentIndex]) {
+				int temp = heap[index];
+				heap[index] = heap[parentIndex];
+				heap[parentIndex] = temp;
+			} else { break; }
+
+			index = parentIndex;
+		}
+	}
+
+	void heap::MinHeap::HeapifyUp() {
+		int index = 0;
+		int leftChildIndex;
+		int rightChildIndex;
+		while (index >= 0){
+			leftChildIndex = 2 * index + 1;
+			rightChildIndex = 2 * index + 2;
+
+			if (heap[leftChildIndex] > heap[index]) {
+				int temp = heap[leftChildIndex];
+				heap[leftChildIndex] = heap[index];
+				heap[index] = temp;
+
+				index = leftChildIndex;
+			} else if (heap[rightChildIndex] > heap[index]) {
+				int temp = heap[rightChildIndex];
+				heap[rightChildIndex] = heap[index];
+				heap[index] = temp;
+
+				index = rightChildIndex;
+			} else { break; }
+		}
+	}
--- DatatypesAndAlgorithms/heap.h
+++ DatatypesAndAlgorithms/heap.h
@@ -1,19 +1,22 @@
 #pragma once
 #include <vector>
 
-class MinHeap
-{
-public:
-	MinHeap(int size);
-	~MinHeap();
-	MinHeap operator[](int index);
-	void Insert(int val);
-	int Delete();
-	int GetLength();
-	void Print();
-private:
-	std::vector<int> heap;
-	void HeapifyUp();
-	void HeapifyDown();
-};
+namespace heap {
+	class MinHeap
+	{
+	public:
+		MinHeap(int size);
+		~MinHeap();
+		MinHeap operator[](int index);
+		void Insert(int val);
+		int Delete();
+		int GetLength();
+		void Print();
+	private:
+		std::vector<int> heap;
+		void HeapifyUp();
+		void HeapifyDown();
+	};
+}
+
 
