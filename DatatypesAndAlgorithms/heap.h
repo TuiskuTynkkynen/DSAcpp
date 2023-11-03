d7b84a3b7b7ade37c2d063d037ac5f1f9eb4cb1c
Tuisku Tynkkynen <tuisku.tynkkynen@gmail.com>
2023-11-03 20:46:50 +0200

    Added Min Heap declerations

--- DatatypesAndAlgorithms/heap.h
+++ DatatypesAndAlgorithms/heap.h
@@ -1,8 +1,19 @@
 #pragma once
+#include <vector>
 
-
-class Heap
+class MinHeap
 {
-
+public:
+	MinHeap(int size);
+	~MinHeap();
+	MinHeap operator[](int index);
+	void Insert(int val);
+	int Delete();
+	int GetLength();
+	void Print();
+private:
+	std::vector<int> heap;
+	void HeapifyUp();
+	void HeapifyDown();
 };
 
